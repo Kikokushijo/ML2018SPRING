8cd28640f80ac56fea104687a3f6427ca7410289
Kikokushijo <zoo30215@gmail.com>
2018-05-03 23:15:14 +0800

    Fix hw4

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -1,8 +1,13 @@
 import sys
+import numpy as np
+
 test_case = sys.argv[2]
 result = sys.argv[3]
 
+with open(test_case, 'r') as f:
+	test = np.loadtxt(f, delimiter=',', skiprows=1)
+
 with open(result, 'w+') as f:
 	f.write('ID,Ans\n')
-	for i in range(1980000):
-		f.write('%d,0\n' % i)
+	for idx, x, y in test:
+		f.write('%d,0\n' % idx)
